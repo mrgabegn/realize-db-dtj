44488ad861d00def05f392e60e97d6fa8b5df27f
GabrielGiuseppe <47945875+GabrielGiuseppe@users.noreply.github.com>
2026-06-08 12:35:10 -0300

    Initial setup

---
+++ src/main/java/realize/db/dtj/DtjApplication.java
@@ -0,0 +1,13 @@
+package realize.db.dtj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/realize/db/dtj/DtjApplicationTests.java
@@ -0,0 +1,13 @@
+package realize.db.dtj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
